9b382058293b6a60e1fc822a8dc6c4aef8909b13
liuxing <1052465416@qq.com>
2021-03-22 15:54:04 +0800

    add function sum2 in file of foo1.cpp

--- f1/foo1.cpp
+++ f1/foo1.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int sum2(int a, int b)
+{
+	return a + 2 * b;
+}
+
 int main()
 {
 	cout << "this is foo1.cpp" << endl;
+
+	cout << "the function sum2 result is " << sum2(12 + 2) << endl;
+
 	return 0;
 }
